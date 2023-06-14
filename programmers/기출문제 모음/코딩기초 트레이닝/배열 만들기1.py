677be8aeffdf16ad9858147d992e20f98a068345
JaehwanLee <96andrew@naver.com>
2023-06-14 22:29:50 +0900

    230614

---
+++ programmers/기출문제 모음/코딩기초 트레이닝/배열 만들기1.py
@@ -0,0 +1,11 @@
+def solution(n, k):
+    answer = []
+    for i in range(1, n + 1):
+        if i % k == 0:
+            answer.append(i)
+    return answer
+
+'''
+def solution(n, k):
+    return [i for i in range(k, n + 1, k)]
+'''
