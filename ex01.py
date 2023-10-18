40c7f4ae15e6445ad315ae2682f3c8d77f8831f0
JaehwanLee <96andrew@naver.com>
2023-10-18 22:47:00 +0900

    20231018

--- ex01.py
+++ ex01.py
@@ -1,19 +1,16 @@
 import sys
 
-input = sys.stdin.readline
+si = sys.stdin.readline
 
-A, B = input().split()
+t = int(si())
 
-def check(A, B):
-    result = sum([A[i] != B[i] for i in range(len(A))])
-    return result
+dp = [[0, 0] for _ in range(41)]
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+dp[2] = [1, 1]
+for i in range(3, 41):
+    dp[i] = [dp[i-2][0] + dp[i-1][0], dp[i-2][1] + dp[i-1][1]]
 
-if len(A) == len(B):
-    print(check(A, B))
-else:
-    result = 50
-    for i in range(len(B)-len(A)+1):
-        temp = check(A, B[i:i+len(A)])
-        if temp < result:
-            result = temp  
-    print(result)
+for i in range(t):
+    n = int(si())
+    print(*dp[n])
